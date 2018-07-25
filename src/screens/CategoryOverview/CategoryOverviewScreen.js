ef08ebd60ea9234f79d1ed1ac495212ed1764aea
Yassine Gherbi <yassine@hexelnet.com>
2018-07-25 14:35:58 +0200

    added a render method that returns just an empty view so the app doesn't break on launch

--- src/screens/CategoryOverview/CategoryOverviewScreen.js
+++ src/screens/CategoryOverview/CategoryOverviewScreen.js
@@ -1,4 +1,14 @@
 import React, { Component } from 'react'
+import { Text, View }       from 'react-native'
 
 export default class CategoryOverviewScreen extends Component {
+
+  render() {
+    return (
+        <View>
+          <Text>CategoryOverviewScreen</Text>
+        </View>
+    )
+  }
+
 }
