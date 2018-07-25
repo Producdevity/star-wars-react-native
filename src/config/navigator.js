cf1b98376af24559c0c071a0ba177440147dd6bf
Yassine Gherbi <yassine@hexelnet.com>
2018-07-25 14:36:50 +0200

    a simple navigator to switch between the 2 screens

---
+++ src/config/navigator.js
@@ -0,0 +1,35 @@
+import { createStackNavigator } from 'react-navigation'
+
+// Utils
+import { is } from '../common/utils'
+
+// Data
+import { ROUTES } from '../common/data'
+
+// Screens
+import CategoryItemsOverviewScreen from '../screens/CategoryItemsOverview/CategoryItemsOverviewScreen'
+import CategoryOverviewScreen      from '../screens/CategoryOverview/CategoryOverviewScreen'
+
+
+// App Routes/Screens
+const routeConfigMap = {
+  [ROUTES.CategoryOverview]:      {
+    screen: CategoryOverviewScreen
+  },
+  [ROUTES.CategoryItemsOverview]: {
+    screen: CategoryItemsOverviewScreen
+  }
+}
+
+
+// Default Route options
+const options = {
+  headerMode:       'none',
+  initialRouteName: ROUTES.CategoryOverview,
+  mode:             is.ios() ? 'modal' : 'card'
+}
+
+
+const AppNavigator = createStackNavigator(routeConfigMap, options)
+
+export default AppNavigator
