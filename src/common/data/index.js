1fe73f2d4e8e05f1dd712bebdd7244aad588c62c
Yassine Gherbi <yassine@hexelnet.com>
2018-07-25 14:22:41 +0200

    added the 2 routes names we are going to use in the `data` dir in a routes.js file

---
+++ src/common/data/index.js
@@ -0,0 +1,6 @@
+/**
+ * Here we export all the constant data we use in the app to stay
+ * consistent and prevent ourselfs from making bugs related to typos
+ */
+
+export * from './routes'
---
+++ src/common/data/routes.js
@@ -0,0 +1,4 @@
+export const ROUTES = {
+  CagtegoryOverview:      'CagtegoryOverview',
+  CagtegoryItemsOverview: 'CagtegoryItemsOverview'
+}
